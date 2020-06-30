3081ce1a56eb9f7500a244485d812f33f0b059f0
윤승권 <gwon@MacBook-Pro.local>
2020-06-30 19:31:13 +0900

    Updates

---
+++ precache-manifest.c17c5daad2e96556d260f5e6f53834ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f106636f217a307159056fb755f400f",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "397af13f927a3cf05aba",
+    "url": "/movie_app_2019/static/css/main.34fe7904.chunk.css"
+  },
+  {
+    "revision": "118469322d9ff5a4cfd2",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.a786e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "397af13f927a3cf05aba",
+    "url": "/movie_app_2019/static/js/main.d36e61bc.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
